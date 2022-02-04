750c4e321245d4750fba46275efba8f8d3ccf88d
Chris Castle <chc@sitecore.net>
2022-02-04 12:48:05 -0600

    audiencs

---
+++ Audience Templates/Viewed x pages in current visit.js
@@ -0,0 +1,30 @@
+(function () {
+  var currentWebSession = getCurrentWebSession(guest);
+  var numOfPageViews = getNumberOfViewEventsInSession(currentWebSession);
+  var numOfTimes = "[[X | number]]";
+  if(numOfPageViews >= numOfTimes){
+      return {};
+  }
+  return false;
+})();
+
+function getCurrentWebSession(guest) {
+    var sessions = guest.sessions;
+    for (var i = 0; i < sessions.length; i++) {
+        if (sessions[i].sessionType === 'WEB' && sessions[i].operatingSystem != null && sessions[i].status === 'OPEN') {
+            return sessions[i];
+        }
+    }
+    return null;
+}
+
+function getNumberOfViewEventsInSession(session) {
+    var numberOfEvents = 0;
+    for (var i = 0; i < session.events.length; i++) {
+        var event = session.events[i];
+        if (event.type === "VIEW") {
+            numberOfEvents++;
+        }
+    }
+    return numberOfEvents;
+}
---
+++ Audience Templates/Visited x times.js
@@ -0,0 +1,14 @@
+// Use server-side JavaScript to filter your audience further
+// You have full access to the guest context which can be accessed under guest, e.g. guest.email
+// Any truthy return value will pass the audience filter, it is recommended to return an object
+// The value returned can be accessed from the variant API response as 'filter'
+
+(function () {
+    let count = `[[X | number]]`
+  
+    if (guest.sessions && guest.sessions.length >= count) 
+      return true;
+    
+    return false;
+  })();
+  
