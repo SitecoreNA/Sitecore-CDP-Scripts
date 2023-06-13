b99d4b76a906821db588705d0321af1cc0e35e74
Richard Flynn <rif@osx-rif-n-my.local>
2023-06-13 11:04:56 +0800

    add v2 engage SDK

---
+++ _TM Script/v2_engage_sdk/engage-sdk-base-script.js
@@ -0,0 +1,76 @@
+// ==UserScript==
+// @name Sitecore CDP/P base script - yoursite
+// @namespace http://tampermonkey.net/
+// @version 0.2
+// @description Generic Load Sitecore CDP Tampermonkey scripts
+// @author Sitecore Presales
+// @match https://*.yoursite.com/*
+// @grant  none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    //Choose the API_TARGET that matches your API region (Europe,USA,or Asia)
+    const EU_API_TARGET = "https://api-engage-eu.sitecorecloud.io";
+    const US_API_TARGET = "https://api-engage-us.sitecorecloud.io";
+    const AP_API_TARGET = "https://api-engage-ap.sitecorecloud.io​";
+
+    //Engage SDK settings
+    const ENGAGE_COOKIE_DOMAIN = ''; //replace with your target website
+    const ENGAGE_API_TARGET = AP_API_TARGET;
+    const ENGAGE_CLIENT_KEY = "";
+    const ENGAGE_POINT_OF_SALE = "StandardDemo";
+
+    const CURRENCY = "USD";
+    const CHANNEL = "WEB"
+    const LANG = "EN";
+
+    main();
+
+    var engage = undefined;
+
+    function main(engage) {
+        console.log("Loading Sitecore Engage SDK...")
+
+        // Create and inject the <script> tag into the HTML
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://d1mj578wat5n4o.cloudfront.net/sitecore-engage-v.1.3.0.min.js";
+        var x = document.querySelector("script");
+        x.parentNode.insertBefore(s, x);
+
+        // Initialize the Engage SDK & send a VIEW event on each page load
+        s.addEventListener("load", async () => {
+            var settings = {
+                clientKey: ENGAGE_CLIENT_KEY,
+                targetURL: ENGAGE_API_TARGET,
+                pointOfSale: ENGAGE_POINT_OF_SALE,
+                cookieDomain: ENGAGE_COOKIE_DOMAIN,
+                cookieExpiryDays: 365,
+                forceServerCookieMode: false,
+                includeUTMParameters: true,
+                webPersonalization: "true"
+            };
+            window.engage = await window.Engage.init(settings);
+
+            var event = {
+                channel: CHANNEL,
+                language: LANG,
+                currency: CURRENCY,
+                page: window.location.pathname + window.location.search
+            };
+            // Send VIEW event
+            window.engage.pageView(event);
+            console.log("Sitecore Engage SDK Sent VIEW event")
+
+            //temp solution for demobars as the Engage.settings object does not expose the cookieDomain
+            localStorage.setItem("scDemoBar_cookieDomain", settings.cookieDomain);
+        });
+    }
+
+
+})();
+
+
